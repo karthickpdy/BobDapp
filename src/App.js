c80b2eb201f9ca7f2661099fd0eeb4a1f238a6f4
Srinivasan <karthickpdy@gmail.com>
2017-10-21 15:48:52 +0530

    Interacting with  customer contract

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import SimpleStorageContract from '../build/contracts/SimpleStorage.json'
+import CustomerKyc from '../build/contracts/CustomerKyc.json'
 import getWeb3 from './utils/getWeb3'
 
 import './css/oswald.css'
@@ -44,25 +44,26 @@ class App extends Component {
      */
 
     const contract = require('truffle-contract')
-    const simpleStorage = contract(SimpleStorageContract)
-    simpleStorage.setProvider(this.state.web3.currentProvider)
+    const customerKyc = contract(CustomerKyc)
+    customerKyc.setProvider(this.state.web3.currentProvider)
 
     // Declaring this for later so we can chain functions on SimpleStorage.
-    var simpleStorageInstance
+    var customerKycInstance
 
     // Get accounts.
     this.state.web3.eth.getAccounts((error, accounts) => {
-      simpleStorage.deployed().then((instance) => {
-        simpleStorageInstance = instance
+      customerKyc.deployed().then((instance) => {
+        customerKycInstance = instance
 
         // Stores a given value, 5 by default.
-        return simpleStorageInstance.set(5, {from: accounts[0]})
+        return customerKycInstance.addCustomer(1,"srini", {from: accounts[0]})
       }).then((result) => {
         // Get the value from the contract to prove it worked.
-        return simpleStorageInstance.get.call(accounts[0])
+        return customerKycInstance.isAadharVerified.call(1)
       }).then((result) => {
+        console.log(result)
         // Update state with the result.
-        return this.setState({ storageValue: result.c[0] })
+        // return this.setState({ storageValue: result.c[0] })
       })
     })
   }
