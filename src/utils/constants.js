d785aed26318f477eb6c667178e051c1b6fcbce8
rajbharath <rbrajbharath1@gmail.com>
2017-11-11 18:51:12 +0530

    logs

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,3 @@
+console.log(process.env);
 export const API_SERVICE_URL = process.env.REACT_APP_API_SERVICE_URL || 'http://127.0.0.1:8082'
 export const ETHEREUM_RPC_URL = process.env.REACT_APP_ETHEREUM_RPC_URL || 'http://localhost:8545' 
