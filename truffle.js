fee4c08ec8c4e3eb1e036a62a466494288124759
rajbharath <rbrajbharath1@gmail.com>
2017-11-11 18:05:34 +0530

    Renamed Ethereum URL & live migration in truffle

--- truffle.js
+++ truffle.js
@@ -7,7 +7,7 @@ module.exports = {
       network_id: "*" // Match any network id
     },
     live: {
-      host: process.env.ETHEREUM_RPC_URL, // Connect to geth on the specified
+      host: process.env.REACT_APP_ETHEREUM_RPC_URL, // Connect to geth on the specified
       port: 8545,
       from: "be8715d3858c837fd08c51f3d0fac8a451225694", // default address to use for any transaction Truffle makes during migrations
       network_id: 15,
