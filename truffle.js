72a84878148e6788ae5286431602f3ed5774786c
rajbharath <rbrajbharath1@gmail.com>
2017-11-11 18:18:52 +0530

    Revert "Removed port"
    This reverts commit 52ae87b57e85154c32737fb773bcbc598b4dbcce.

--- truffle.js
+++ truffle.js
@@ -8,6 +8,7 @@ module.exports = {
     },
     live: {
       host: process.env.NODE_CONFIG_TRUFFLE_CONFIG_ETHEREUM_RPC_URL, // Connect to geth on the specified
+      port: 8545,
       from: "be8715d3858c837fd08c51f3d0fac8a451225694", // default address to use for any transaction Truffle makes during migrations
       network_id: 15,
       gas: 4700000 // Gas limit used for deploys
